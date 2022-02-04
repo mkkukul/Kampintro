7bab56b359cdf77843954d5dbee064ef01e5fd4f
KukulX <KukulX@DESKTOP-LE14B3K>
2022-02-04 09:38:51 +0300

    Add project files.

---
+++ Kampintro/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Kampintro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güveliği
+            
+            
+            
+            string KategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistememGirisYapmismi = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalıs butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("değişmedi butonu");
+            }            
+
+
+
+
+            if (sistememGirisYapmismi == true)
+            {
+                Console.WriteLine("kullancı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(KategoriEtiketi);
+            Console.WriteLine(KategoriEtiketi);
+
+            Console.WriteLine(KategoriEtiketi);
+
+            Console.WriteLine(KategoriEtiketi);
+
+            Console.WriteLine(KategoriEtiketi);
+        }
+    }
+}
